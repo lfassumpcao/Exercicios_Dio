04b567b2badc3173311f22d0bebec27fc4889d83
Luis Felipe Assumpção <lfassumpcao@gmail.com>
2025-09-06 23:49:16 -0300

    Add files via upload

---
+++ 001-desafio-surpresa-logica.js
@@ -0,0 +1,17 @@
+let pokemon = [
+["Poochyena","lv2","macho","13/13"],
+["Zigzagoon","lv2","fêmea","13/13"],
+["Dragonite","lv5","macho","25/25"],
+["Dragonite","lv5","fêmea","24/24"],
+["Dragonite","lv5","fêmea","24/24"],
+["Poochyena","lv3","fêmea","15/15"],
+["Wurmple","lv2","macho","7/14"]
+]
+
+console.log("O pokemon " + pokemon[0][0] + ", " + pokemon[0][1] + ", " + pokemon[0][2] + ", " + pokemon[0][3] +" foi cadastrado com sucesso!")
+console.log("O pokemon " + pokemon[1][0] + ", " + pokemon[1][1] + ", " + pokemon[1][2] + ", " + pokemon[1][3] +" foi cadastrado com sucesso!")
+console.log("O pokemon " + pokemon[2][0] + ", " + pokemon[2][1] + ", " + pokemon[2][2] + ", " + pokemon[2][3] +" foi cadastrado com sucesso!")
+console.log("O pokemon " + pokemon[3][0] + ", " + pokemon[3][1] + ", " + pokemon[3][2] + ", " + pokemon[3][3] +" foi cadastrado com sucesso!")
+console.log("O pokemon " + pokemon[4][0] + ", " + pokemon[4][1] + ", " + pokemon[4][2] + ", " + pokemon[4][3] +" foi cadastrado com sucesso!")
+console.log("O pokemon " + pokemon[5][0] + ", " + pokemon[5][1] + ", " + pokemon[5][2] + ", " + pokemon[5][3] +" foi cadastrado com sucesso!")
+console.log("O pokemon " + pokemon[6][0] + ", " + pokemon[6][1] + ", " + pokemon[6][2] + ", " + pokemon[6][3] +" foi cadastrado com sucesso!")
